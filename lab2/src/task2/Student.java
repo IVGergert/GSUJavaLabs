8bedb9258b8fcc86e2d3b06f680b399025b09709
Ilya Gergert <IlyaGergert@gmail.com>
2025-06-20 14:27:07 +0300

    Lab2

---
+++ lab2/src/task2/Student.java
@@ -0,0 +1,14 @@
+package task2;
+
+public class Student {
+
+    String lastName;
+    int age;
+    boolean arrears;
+
+    public Student(String lastName, int age, boolean arrears){
+        this.lastName = lastName;
+        this.age = age;
+        this.arrears = arrears;
+    }
+}
